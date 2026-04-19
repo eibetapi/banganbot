782d7b29f0cb0986022215e47dece05ca31f0a7e
eibetapi <contatoelizaps@gmail.com>
2026-04-19 15:22:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -974,5 +974,5 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        print("\n🛸 Desligando motores...")
+        print("\n🛸 Desligando motores...")  
 
